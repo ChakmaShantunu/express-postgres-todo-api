e4998033ad7ae8f5474754c26fcf8fed4c39519c
shantunu007 <chakmashantunu.web@gmail.com>
2025-12-09 19:06:41 +0600

    add POST API endpoint and configure PostgreSQL connection using pg & Neon DB

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,29 @@
-import express, { Request, Response } from "express";
+import express, { request, Request, Response } from "express";
+import { Pool } from "pg"
 
 const app = express()
 const port = 5000;
 
+const pool = new Pool({
+    connectionString: `postgresql://neondb_owner:npg_oW0eSZPaV8UM@ep-soft-mud-a4ehym3k-pooler.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require`
+});
+
+// Parser
+app.use(express.json());
+
 app.get('/', (req: Request, res: Response) => {
     res.send('Hello Boss! Welcome to the server. Next level')
-})
+});
+
+app.post("/", (req: Request, res: Response) => {
+    console.log(req.body);
+
+    res.status(201).json({
+        success: true,
+        message: "API is working"
+    })
+});
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
-})
+});
